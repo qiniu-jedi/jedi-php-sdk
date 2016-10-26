0bccc2e8134f12b305710129e45114d633339437
Jemy <jemygraw@gmail.com>
2016-10-26 11:40:24 +0800

    add get all video list example

--- examples/getAllVideoList.php
+++ examples/getAllVideoList.php
@@ -1,3 +1,40 @@
 <?php
 
 include_once 'config.php';
+
+use Qiniu\Jedi;
+
+$jediAuth = new Jedi\JediAuth($ak, $sk);
+$jediManager = new Jedi\JediManager($jediAuth);
+
+
+$count = 3;
+
+$result = $jediManager->getVideoList($hub, $count);
+if (!empty($result)) {
+    $cursor = $result['videoList']['cursor'];
+    $totalCount = $result['videoList']['count'];
+    $videoItems = $result['videoList']['items'];
+    foreach ($videoItems as $videoItem) {
+        //output video info
+        print($videoItem['key'] . "\t" . $videoItem['name'] . "\n");
+    }
+
+    while (!empty($cursor)) {
+        $result = $jediManager->getVideoList($hub, $count, $cursor);
+        if (!empty($result)) {
+            $cursor = $result['videoList']['cursor'];
+            $totalCount = $result['videoList']['count'];
+            $videoItems = $result['videoList']['items'];
+            foreach ($videoItems as $videoItem) {
+                //output video info
+                print($videoItem['key'] . "\t" . $videoItem['name'] . "\n");
+            }
+        } else {
+            print_r($result['response']);
+            break;
+        }
+    }
+} else {
+    print_r($result['response']);
+}
